f74a7e19e6031a01a270e5704ba07b6ee7d32716
Madhav Goyal <88841339+hydrogenbond007@users.noreply.github.com>
2023-03-26 13:26:23 +0530

    creating tree.cr
    just a poc not tested yet

---
+++ tree.cr
@@ -0,0 +1,26 @@
+include "circuits/utils.circom";
+include "circuits/merkle.circom";
+//make sure to include these libraries in the cargo.toml file
+
+template MerkleTree() {
+  signal leaves[256];
+  signal index;
+  signal root;
+  signal path[256];
+
+  component arrayLength = ArrayLength(256)();
+  component merkleTree = MerkleTree(256)();
+
+  merkleTree.input.leaves <== leaves;
+  merkleTree.input.index <== index;
+  root <== merkleTree.output.root;
+  path <== merkleTree.output.path;
+
+  // Constraints
+  enforceArrayConstraint(leaves);
+  enforceIntegerConstraint(index, 0, arrayLength.output);
+  enforceIntegerConstraint(root);
+  enforceArrayConstraint(path);
+}
+
+component main = MerkleTree();
